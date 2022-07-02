3ff23baaab48b5011faba5f4f914225066aa4353
crowguy424 <deepto.osman@gmail.com>
2022-07-02 08:52:58 -0600

    first commit

---
+++ Tuts1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	//clrscr();
+	cout << "I love you" << endl;
+	system("pause");
+	getch();
+}
---
+++ Tuts2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	int x;
+	cout << "Integer: ";
+	cin >> x;
+	if(x%2==0){
+		cout << "Number is even";
+	} else{
+		cout << "Number is odd";
+	}
+	system("pause>0");
+	return 0;
+}
